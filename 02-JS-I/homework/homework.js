75ccc419684821d0c7720ec4a1c43f2e2d10c5c1
Juan <juan.daniel.ruiz.diaz@gmail.com>
2021-11-17 22:50:31 -0300

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -170,8 +170,8 @@ function esPositivo(numero) {
 
   if (numero > 0) {
     return 'Es positivo';
-  } else if (numero <0) {
-    return ('Es negativo');
+  } else if (numero < 0) {
+    return 'Es negativo';
   }
   else (numero === 0) 
   return false; 
@@ -204,20 +204,23 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return alto * ancho
+    
+
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado * 4
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  return base * altura / 2
 
 }
 
@@ -226,7 +229,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+   return euro * 1.20
+    
 }
 
 
@@ -236,7 +240,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+    if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+    return'Es vocal'
+   } else if (letra.length !== 1) {
+    return 'Dato incorrecto';
+   }
+   else 
+    return 'Dato incorrecto';
 }
 
 
